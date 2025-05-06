eac7b4bcf79ef954a679ee2c607695d71c7a62b3
Odyssey <odyssey409@naver.com>
2025-05-06 22:19:33 +0900

    lamp 버전

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,9 @@
+import { LampDemo } from "@/components/ui/lamp-demo";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen w-full">
+      <LampDemo />
+    </div>
+  );
 }
